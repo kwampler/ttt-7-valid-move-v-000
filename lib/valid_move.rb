d4449c7566c62f718c55446c24790d89d360c4b9
katejsenger <katejsenger@yahoo.com>
2018-10-02 02:48:58 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -6,11 +6,8 @@ def position_taken?(board, index)
   end
 end
 def valid_move?(board, index)
-<<<<<<< HEAD
   if index.between?(0, 8) && position_taken?(board, index) == false
-=======
   if index.between?(0, 8) && position_taken? == false
->>>>>>> 72708f68f5cb499a7f37f39dcf8f178a60a2b19e
     return true
   else
     return false 
